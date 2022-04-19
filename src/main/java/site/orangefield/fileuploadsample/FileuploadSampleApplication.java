da7423c6c6d7e8e5952412bbdf44a733bbe22321
field <jsw_777@naver.com>
2022-04-19 14:23:45 +0900

    Initial Project

---
+++ src/main/java/site/orangefield/fileuploadsample/FileuploadSampleApplication.java
@@ -0,0 +1,13 @@
+package site.orangefield.fileuploadsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadSampleApplication.class, args);
+	}
+
+}
